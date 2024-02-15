885864c81dcc311d940e56bd0cb718bcdf36818f
Easi-d-Hatchett <easi.hatchett3@gmail.com>
2024-02-15 09:34:47 -0500

    ended second tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { puppyList } from './data.js'
 import { useState } from 'react'
 
 import './App.css'
 
 function App() {
+  const [puppies,setPuppies]=
+  useState(puppyList)
+  
+  console.log(puppies)
   
 
   return (
     <>
       <div>
-        
+        {
+           puppies.map((puppy) => {
+            return <p>{puppy.name}</p>
+          })
+        }
       </div>
      
     </>
