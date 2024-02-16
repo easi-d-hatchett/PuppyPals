3e968834f8ff00a43abb7273a49c02f9dc1113e4
Easi-d-Hatchett <easi.hatchett3@gmail.com>
2024-02-15 19:03:09 -0500

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,41 @@
 import { puppyList } from './data.js'
 import { useState } from 'react'
+import './index.css'
 
 import './App.css'
 
 function App() {
-  const [puppies,setPuppies]=
-  useState(puppyList)
-  
-  console.log(puppies)
-  
 
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick() {
+    // some logic here
+  }
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
   return (
-    <>
-      <div>
-        {
-           puppies.map((puppy) => {
-            return <p>{puppy.name}</p>
-          })
-        }
-      </div>
-     
-    </>
+    <div className="App">
+      {puppies.map((puppy) => {
+       
+       return (
+          <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>
+        {puppy.name}
+      </p>
+        );
+      })}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+    </div>
   )
 }
-
+</div>
+  );
+}
+    
 export default App
